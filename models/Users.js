d11b968b0e21173639191830689bbe95d563ad3c
Jaswal1p <Jaswal1p@yahoo.com>
2022-03-27 03:34:25 -0400

    added code in models files to connect thoughts.js with Users.js

--- models/Users.js
+++ models/Users.js
@@ -13,12 +13,18 @@ const UsersSchema = new Schema(
             required: true,
             unique: true,
             // here I wil use RegEx to validate eneterd email, as learned in the class activity and RegEx created in assignment 17
-        }
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]{2,6})$/]
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts' 
+        }]
 
     },
     {
         toJSON: {
             virtuals: true,
+            getters: true,
         },
         id: false
     }
