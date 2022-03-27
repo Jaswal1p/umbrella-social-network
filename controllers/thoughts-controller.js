3d001fd18d33b5a79db72566d2058430ba8242e3
Jaswal1p <Jaswal1p@yahoo.com>
2022-03-27 05:43:28 -0400

    adeded code for add & delete Reaction to thoughts in the thought-controller.js

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -100,7 +100,7 @@ const thoughtsController = {
 
         .then(dbThoughtsData => {
             if (!dbThoughtsData) {
-                res.status(404).json({message: 'No thoughts with this particular ID!'});
+                res.status(404).json({message: 'There is no thought with this particular id!'});
                 return;
             }
             res.json(dbThoughtsData);
@@ -108,7 +108,37 @@ const thoughtsController = {
         .catch(err => res.status(400).json(err));
     },
 
+    // Add a Reaction to the user comment
+    addReaction({params, body}, res) {
+        Thoughts.findOneAndUpdate({_id: params.thoughtId}, {$push: {reactions: body}}, {new: true, runValidators: true})
+            .populate({
+                path: 'reactions',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbThoughtsData => {
+                if (!dbThoughtsData) {
+                    res.status(404).json({message: 'There is no thought with this particular id!'});
+                    return;
+                }
+                res.json(dbThoughtsData);
+            })
+            .catch(err => res.status(400).json(err))
 
+    },
+
+    // Delete a reaction by id
+    deleteReaction({params}, res) {
+        Thoughts.findOneAndUpdate({_id: params.thoughtId}, {$pull: {reactions: {reactionId: params.reactionId}}}, {new : true})
+            .then(dbThoughtsData => {
+                if (!dbThoughtsData) {
+                    res.status(404).json({message: 'No thoughts with this particular ID!'});
+                    return;
+                }
+                res.json(dbThoughtsData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
 
 };
 
