d0171acc168d106b29df721a5978b4f6705c818c
Jaswal1p <Jaswal1p@yahoo.com>
2022-03-27 00:46:45 -0400

    added code for get all and get one User in users-controller.js

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -9,7 +9,35 @@ const usersController = {
             .catch(err => res.status(400).json(err));
     },
 
+    // get all users
+    getAllUsers(req, res) {
+        Users.find({})
+
+          .then(dbUsersData => res.json(dbUsersData))
+          .catch(err => {
+              console.log(err);
+              res.status(404).json(err);
+          });
+    },
     
+    // get single user by id
+    getUsersById({params}, res) {
+        Users.findOne({_id: params.id })
+          
+          .then(dbUsersData => {
+              if(!dbUsersData) {
+                  res.status(404).json({message: 'No User with this id!'});
+                  return;
+              }
+              res.json(dbUsersData) 
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+    },
+
+
 };
 
 module.exports = usersController;
